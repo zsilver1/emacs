080a3a5a01a6ad958a3926bcfa95eb8c47a783b5
Zach Silver <zsilver96@gmail.com>
2019-11-27 11:30:46 -0500

    changes

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 ;; Ask "y" or "n" instead of "yes" or "no"
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(set-default-font "Courier 15" nil t)
+(set-default-font "Courier 16" nil t)
 ;; get rid of right fringe
 (set-face-attribute 'fringe nil :background nil)
 
@@ -80,11 +80,14 @@
 ;; Deletes selected region
 (delete-selection-mode 1)
 
+(hl-line-mode 1)
+
 ;; Make all file names unique
 (require 'uniquify)
 
 ;; Electric Pair mode
 (electric-pair-mode 1)
+
 
 (defun smart-line-beginning ()
   "Move point to the beginning of text on the current line; if that is already
@@ -101,20 +104,20 @@ the current position of point, then move it to the beginning of the line."
 (setq org-hide-leading-stars t)
 
 ;; MODE LINE
-(setq-default mode-line-format
-              '("%e"
-                mode-line-front-space
-                mode-line-modified
-                " "
-                default-directory
-                mode-line-buffer-identification
-                " "
-                mode-line-position
-                (vc-mode vc-mode)
-                (flycheck-mode flycheck-mode-line)
-                " "
-                mode-line-misc-info
-                mode-line-end-spaces))
+;; (setq-default mode-line-format
+;;               '("%e"
+;;                 mode-line-front-space
+;;                 mode-line-modified
+;;                 " "
+;;                 default-directory
+;;                 mode-line-buffer-identification
+;;                 " "
+;;                 mode-line-position
+;;                 (vc-mode vc-mode)
+;;                 (flycheck-mode flycheck-mode-line)
+;;                 " "
+;;                 mode-line-misc-info
+;;                 mode-line-end-spaces))
 
 
 ;; KEYBINDINGS
@@ -136,6 +139,31 @@ the current position of point, then move it to the beginning of the line."
   :config
   (load-theme 'base16-default-dark t))
 
+(use-package worf
+  :config
+  ;; use ivy to insert a link to a heading in the current document
+  ;; based on `worf-goto`
+  (defun zs/worf-insert-internal-link ()
+    "Use ivy to insert a link to a heading in the current `org-mode' document. Code is based on `worf-goto'."
+    (interactive)
+    (let ((cands (worf--goto-candidates)))
+      (ivy-read "Heading: " cands
+                :action 'zs/worf-insert-internal-link-action)))
+  (defun zs/worf-insert-internal-link-action (x)
+    "Insert link for `zs/worf-insert-internal-link'"
+    ;; go to heading
+    (save-excursion
+      (goto-char (cdr x))
+      ;; store link
+      (call-interactively 'org-store-link)
+      )
+    ;; return to original point and insert link
+    (org-insert-last-stored-link 1)
+    ;; org-insert-last-stored-link adds a newline so delete this
+    (delete-backward-char 1)
+    )
+  (define-key org-mode-map (kbd "C-c C-l") 'zs/worf-insert-internal-link))
+
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
   :config
@@ -147,10 +175,12 @@ the current position of point, then move it to the beginning of the line."
 
 (use-package expand-region
   :bind ("C-j" . er/expand-region)
-  :config
+  :init (define-key org-mode-map "\C-j" 'er/expand-region)
+:config
   (setq shift-select-mode nil))
 
 (use-package counsel
+  :demand
   :bind (("C-;" . counsel-imenu)
          ("C-c s" . swiper)
          ("C-c C-s" . swiper)
@@ -163,9 +193,15 @@ the current position of point, then move it to the beginning of the line."
          ("C-c r" . counsel-recentf)
          (:map counsel-find-file-map
                ("RET" . ivy-alt-done)
-               ("C-j" . ivy-immediate-done)))
+               ("C-j" . ivy-immediate-done))
+         ;; (:map org-mode-map
+         ;;       ("C-s" . swiper))
+         )
   :config
   (ivy-mode 1)
+  (add-hook 'org-mode-hook
+  (lambda ()
+    (define-key org-mode-map "\C-s" 'swiper)))
   (setq ivy-use-virtual-buffers t)
   (setq ivy-initial-inputs-alist nil)
   (setq ivy-extra-directories nil)
@@ -247,16 +283,139 @@ the current position of point, then move it to the beginning of the line."
    ("C-c t" . crux-visit-term-buffer)
    ("C-c C" . crux-copy-file-preserve-attributes)))
 
+
+(setq org-publish-project-alist
+      '(("Shetlandic"
+         :base-directory "~/Documents/Constructed/Shetlandic/"
+         :publishing-directory "~/Documents/Constructed/Shetlandic/static"
+         :publishing-function org-html-publish-to-html
+         :exclude "proto.*")))
+
+
+(use-package dash-functional)
+
+
+(define-minor-mode unpackaged/org-export-html-with-useful-ids-mode
+  "Attempt to export Org as HTML with useful link IDs.
+Instead of random IDs like \"#orga1b2c3\", use heading titles,
+made unique when necessary."
+  :global t
+  (if unpackaged/org-export-html-with-useful-ids-mode
+      (progn
+        (advice-add #'org-export-new-title-reference :override #'unpackaged/org-export-new-title-reference)
+        (advice-add #'org-export-get-reference :override #'unpackaged/org-export-get-reference))
+    (advice-remove #'org-export-new-title-reference #'unpackaged/org-export-new-title-reference)
+    (advice-remove #'org-export-get-reference #'unpackaged/org-export-get-reference)))
+
+(defun unpackaged/org-export-get-reference (datum info)
+  "Like `org-export-get-reference', except uses heading titles instead of random numbers."
+  (let ((cache (plist-get info :internal-references)))
+    (or (car (rassq datum cache))
+        (let* ((crossrefs (plist-get info :crossrefs))
+               (cells (org-export-search-cells datum))
+               ;; Preserve any pre-existing association between
+               ;; a search cell and a reference, i.e., when some
+               ;; previously published document referenced a location
+               ;; within current file (see
+               ;; `org-publish-resolve-external-link').
+               ;;
+               ;; However, there is no guarantee that search cells are
+               ;; unique, e.g., there might be duplicate custom ID or
+               ;; two headings with the same title in the file.
+               ;;
+               ;; As a consequence, before re-using any reference to
+               ;; an element or object, we check that it doesn't refer
+               ;; to a previous element or object.
+               (new (or (cl-some
+                         (lambda (cell)
+                           (let ((stored (cdr (assoc cell crossrefs))))
+                             (when stored
+                               (let ((old (org-export-format-reference stored)))
+                                 (and (not (assoc old cache)) stored)))))
+                         cells)
+                        (when (org-element-property :raw-value datum)
+                          ;; Heading with a title
+                          (unpackaged/org-export-new-title-reference datum cache))
+                        ;; NOTE: This probably breaks some Org Export
+                        ;; feature, but if it does what I need, fine.
+                        (org-export-format-reference
+                         (org-export-new-reference cache))))
+               (reference-string new))
+          ;; Cache contains both data already associated to
+          ;; a reference and in-use internal references, so as to make
+          ;; unique references.
+          (dolist (cell cells) (push (cons cell new) cache))
+          ;; Retain a direct association between reference string and
+          ;; DATUM since (1) not every object or element can be given
+          ;; a search cell (2) it permits quick lookup.
+          (push (cons reference-string datum) cache)
+          (plist-put info :internal-references cache)
+          reference-string))))
+
+(defun unpackaged/org-export-new-title-reference (datum cache)
+  "Return new reference for DATUM that is unique in CACHE."
+  (cl-macrolet ((inc-suffixf (place)
+                             `(progn
+                                (string-match (rx bos
+                                                  (minimal-match (group (1+ anything)))
+                                                  (optional "--" (group (1+ digit)))
+                                                  eos)
+                                              ,place)
+                                ;; HACK: `s1' instead of a gensym.
+                                (-let* (((s1 suffix) (list (match-string 1 ,place)
+                                                           (match-string 2 ,place)))
+                                        (suffix (if suffix
+                                                    (string-to-number suffix)
+                                                  0)))
+                                  (setf ,place (format "%s--%s" s1 (cl-incf suffix)))))))
+    (let* ((title (org-element-property :raw-value datum))
+           (ref (url-hexify-string (substring-no-properties title)))
+           (parent (org-element-property :parent datum)))
+      (while (--any (equal ref (car it))
+                    cache)
+        ;; Title not unique: make it so.
+        (if parent
+            ;; Append ancestor title.
+            (setf title (concat (org-element-property :raw-value parent)
+                                "--" title)
+                  ref (url-hexify-string (substring-no-properties title))
+                  parent (org-element-property :parent parent))
+          ;; No more ancestors: add and increment a number.
+          (inc-suffixf ref)))
+      ref)))
+
+
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+
+(add-hook 'org-mode-hook 'unpackaged/org-export-html-with-useful-ids-mode)
+(add-hook 'org-mode-hook 'auto-fill-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(ansi-term-color-vector
+   [unspecified "#f8f8f8" "#ab4642" "#a1b56c" "#f7ca88" "#7cafc2" "#ba8baf" "#7cafc2" "#383838"] t)
  '(company-box-enable-icon nil)
+ '(custom-enabled-themes (quote (base16-default-dark)))
+ '(custom-safe-themes
+   (quote
+    ("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "2a998a3b66a0a6068bcb8b53cd3b519d230dd1527b07232e54c8b9d84061d48d" default)))
  '(lsp-ui-sideline-enable nil)
+ '(org-cycle-hook
+   (quote
+    (org-cycle-hide-archived-subtrees org-cycle-show-empty-lines org-optimize-window-after-visibility-change)))
  '(package-selected-packages
    (quote
-    (crux cquery lsp-python company-lsp lsp-ui pipenv spaceline company-box eglot company dashboard counsel-etags undo-tree smex which-key use-package ido-vertical-mode ido-completing-read+ flx-ido expand-region exec-path-from-shell esup counsel base16-theme))))
+    (dash-functional worf yasnippet flycheck-rust cargo dumb-jump rust-mode crux cquery lsp-python company-lsp lsp-ui pipenv spaceline company-box eglot company dashboard counsel-etags undo-tree smex which-key use-package ido-vertical-mode ido-completing-read+ flx-ido expand-region exec-path-from-shell esup counsel base16-theme)))
+ '(unpackaged/org-export-html-with-useful-ids-mode t)
+ '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
